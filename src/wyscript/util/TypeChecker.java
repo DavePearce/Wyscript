01c4ad25d11ae1f06a0547d33fb9eb918adcefc7
Mate Antunovic <just.mate.antunovic@gmail.com>
2013-12-11 10:47:27 +1300

    got Dan to fix bug in type checker check(return) method

--- src/wyscript/util/TypeChecker.java
+++ src/wyscript/util/TypeChecker.java
@@ -84,6 +84,8 @@ public class TypeChecker {
 			check((Stmt.IfElse) stmt, environment);
 		} else if(stmt instanceof Stmt.OldFor) {
 			check((Stmt.OldFor) stmt, environment);
+		} else if (stmt instanceof Stmt.ParFor) {
+			check((Stmt.ParFor) stmt, environment);
 		} else if(stmt instanceof Stmt.For) {
 			check((Stmt.For) stmt, environment);
 		}	else if(stmt instanceof Stmt.While) {
@@ -104,6 +106,10 @@ public class TypeChecker {
 	}
 
 	public void check(Stmt.Return stmt, Map<String, Type> environment) {
+		if (stmt.getExpr() == null) {
+			checkSubtype(function.ret, new Type.Void(), stmt.getExpr());
+			return;
+		}
 		Type actual = check(stmt.getExpr(), environment);
 		checkSubtype(function.ret, actual, stmt.getExpr());
 	}
@@ -145,6 +151,18 @@ public class TypeChecker {
 	}
 
 	public void check(Stmt.For stmt, Map<String, Type> environment) {
+		Expr e = stmt.getSource();
+		Expr.Variable v = stmt.getIndex();
+
+		Type t = check(e, environment);
+		if (!(t instanceof Type.List))
+			syntaxError("For loop source expression must evaluate to a list type", file.filename, e);
+
+		HashMap<String, Type> newEnv = new HashMap<String, Type>(environment);
+		newEnv.put(v.getName(), ((Type.List)t).getElement());
+		check(stmt.getBody(), newEnv);
+	}
+	public void check(Stmt.ParFor stmt, Map<String, Type> environment) {
 		Expr e = stmt.getSource();
 		Expr.Variable v = stmt.getIndex();
 
