5db5e4eedd3c53fc1c77919bc9e88906ddaab569
Mate Antunovic <just.mate.antunovic@gmail.com>
2013-12-20 10:43:59 +1300

    cleaned

--- partests/runtests/main0.cu
+++
@@ -1,6 +0,0 @@
-extern "C" __global__ void main0 ( int* b , int* b_length , int* a , int* a_length ) {
- int i = blockIdx.x * blockDim.x + threadIdx.x ;
- if (! ( i < (*b_length) && i < (*a_length) ) ) {
- return ;
- } b [i] = a [i] ;
- } 
